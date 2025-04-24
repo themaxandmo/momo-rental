982a073667f7bec9438f06f72762f78f895b82c7
Sophie Langis <sophielangis@Mac.attlocal.net>
2025-04-23 21:16:23 -0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MomosRental from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MomosRental />
+  </React.StrictMode>
+)
