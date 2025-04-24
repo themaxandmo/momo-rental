86a42511460e9beada5a1608911d897e0e3e9d25
Sophie Langis <sophielangis@Mac.attlocal.net>
2025-04-23 21:39:30 -0700

    Remove manual input from Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'
-    }
+    outDir: 'dist'
   }
 })
